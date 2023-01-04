6335409b316438661e0023af8b743bd125636994
ronenkat <ronenkat@il.ibm.com>
2023-01-04 09:52:26 +0200

    Create image for sky to run on Docker or k8s Includes Dockerfile and scripts to generate ssh key and build the image using Docker

--- sky/backends/k8s-support/create-oci-image/build-image.sh
+++ sky/backends/k8s-support/create-oci-image/build-image.sh
@@ -2,4 +2,4 @@
 ssh-keygen -q -t ecdsa -f sky-ray
 export SKY_RAY_PUBLIC_KEY=`cat sky-ray.pub`
 
-docker build -t sky-container -f image/Dockerfile --build-arg SKY_RAY_PUBLIC_KEY="$SKY_RAY_PUBLIC_KEY" image/
+docker build -t sky-container --build-arg SKY_RAY_PUBLIC_KEY="$SKY_RAY_PUBLIC_KEY" image/
